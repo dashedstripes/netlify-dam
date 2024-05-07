aada5673131c28dd24a047f8ca74231fce586f94
Adam Gray <adam.gray@netlify.com>
2024-05-07 09:38:36 -0500

    show image in list

--- scripts/getimages.js
+++ scripts/getimages.js
@@ -8,7 +8,15 @@
 
   for (const image of images) {
     const node = document.createElement("li");
-    node.textContent = image.key;
+    const a = document.createElement("a");
+    const img = document.createElement("img");
+
+    img.src = `/.netlify/images?url=/.netlify/functions/getSingleImage?key=${image.key}&w=50&h=50`;
+    a.href = `/.netlify/images?url=/.netlify/functions/getSingleImage?key=${image.key}&w=50&h=50`;
+    a.textContent = image.key;
+
+    a.appendChild(img);
+    node.appendChild(a);
     container.appendChild(node);
   }
 })();
