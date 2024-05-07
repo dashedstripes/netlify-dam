5888ef3d865402990a3afba300828fc15614769e
Adam Gray <adam.gray@netlify.com>
2024-05-07 09:50:05 -0500

    remove image cdn from link

--- scripts/getimages.js
+++ scripts/getimages.js
@@ -1,22 +1,26 @@
 (async () => {
-  const container = document.getElementById("images");
+  try {
+    const container = document.getElementById("images");
 
-  const res = await fetch("/.netlify/functions/getImages");
-  const json = await res.json();
+    const res = await fetch("/.netlify/functions/getImages");
+    const json = await res.json();
 
-  const images = json?.images;
+    const images = json?.images;
 
-  for (const image of images) {
-    const node = document.createElement("li");
-    const a = document.createElement("a");
-    const img = document.createElement("img");
+    for (const image of images) {
+      const node = document.createElement("li");
+      const a = document.createElement("a");
+      const img = document.createElement("img");
 
-    img.src = `/.netlify/images?url=/.netlify/functions/getSingleImage?key=${image.key}&w=50&h=50`;
-    a.href = `/.netlify/images?url=/.netlify/functions/getSingleImage?key=${image.key}&w=50&h=50`;
-    a.textContent = image.key;
+      img.src = `/.netlify/images?url=/.netlify/functions/getSingleImage?key=${image.key}&w=50&h=50`;
+      a.href = `/.netlify/functions/getSingleImage?key=${image.key}`;
+      a.textContent = image.key;
 
-    a.appendChild(img);
-    node.appendChild(a);
-    container.appendChild(node);
+      a.appendChild(img);
+      node.appendChild(a);
+      container.appendChild(node);
+    }
+  } catch (err) {
+    console.error(err);
   }
 })();
